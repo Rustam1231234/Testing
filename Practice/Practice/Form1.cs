cca33b86f5ae0026ceae3c17972989caac7b3fa0
Rustam <otum@bk.ru>
2023-02-15 16:21:12 +0400

    update!

--- Practice/Practice/Form1.cs
+++ Practice/Practice/Form1.cs
@@ -21,5 +21,10 @@ namespace Practice
         {
             MessageBox.Show("123");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("14343");
+        }
     }
 }
