6ad26f88d1b13e5dc5bb0e730418f21fcc0422dd
Rustam <otum@bk.ru>
2023-02-15 16:38:56 +0400

    version ultraMegaGiperLast!(v.2.0)Final2

---
+++ Practice/Practice/Frukts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Frukts
+    {
+        public string name { get; set; }
+        public string description { get; set; } 
+        public double price { get; set; }
+    }
+}
